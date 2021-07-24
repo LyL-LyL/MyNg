ff5a6a9d7bda53dfcd5385bedb9b60ec847b7a27
刘艳领 <zxc12345>
2021-07-24 19:01:22 +0800

    属性绑定

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'myng';
   msg = '我<em>爱</em>我的祖国';
+  // 属性绑定
+  url = 'https://image.baidu.com/search/detail?ct=503316480&z=0&ipn=d&word=%E5%9B%BE%E7%89%87&step_word=&hs=0&pn=17&spn=0&di=161480&pi=0&rn=1&tn=baiduimagedetail&is=0%2C0&istype=0&ie=utf-8&oe=utf-8&in=&cl=2&lm=-1&st=undefined&cs=2021795008%2C2017612367&os=4145246761%2C1244653403&simid=4158460153%2C619649712&adpicid=0&lpn=0&ln=1738&fr=&fmq=1627123344569_R&fm=&ic=undefined&s=undefined&hd=undefined&latest=undefined&copyright=undefined&se=&sme=&tab=0&width=undefined&height=undefined&face=undefined&ist=&jit=&cg=&bdtype=0&oriquery=&objurl=https%3A%2F%2Fgimg2.baidu.com%2Fimage_search%2Fsrc%3Dhttp%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201804%2F28%2F20180428114906_ulvqd.jpg%26refer%3Dhttp%3A%2F%2Fb-ssl.duitang.com%26app%3D2002%26size%3Df9999%2C10000%26q%3Da80%26n%3D0%26g%3D0n%26fmt%3Djpeg%3Fsec%3D1629723238%26t%3D6b34f55970d553b960415d0bd45dc074&fromurl=ippr_z2C%24qAzdH3FAzdH3Fooo_z%26e3B17tpwg2_z%26e3Bv54AzdH3Fks52AzdH3F%3Ft1%3Dl8bbam00m&gsm=12&rpstart=0&rpnum=0&islist=&querylist=&nojc=undefined';
 }
